6f43abea466a9cb159d1d8c14763c7d25d1c32ec
Callum Locke <callum.locke@gmail.com>
2014-01-20 14:38:28 +0000

    return whole Hogan template object, not just render method

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var hogan = require('hogan.js'),
 var filenamePattern = /\.(html|hogan|hg|mustache|ms)$/
 
 var wrap = function(template) {
-    return 'module.exports = new (require(\'hogan.js/lib/template\').Template)(' + template + ').render;'
+    return 'module.exports = new (require(\'hogan.js/lib/template\').Template)(' + template + ');'
 }
 
 module.exports = function(file) {
